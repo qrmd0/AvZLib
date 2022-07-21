3a949a8b713fab8442b10992c1825cd9a5883d4f
Rottenham <54915258+Rottenham@users.noreply.github.com>
2022-07-21 16:35:33 +0800

    fix example bug

--- crescendo/avz-testing/examples/完整阵型炸率测试（以PE靠天无为例）/PE靠天无炮炸率测试.cpp
+++ crescendo/avz-testing/examples/完整阵型炸率测试（以PE靠天无为例）/PE靠天无炮炸率测试.cpp
@@ -7,8 +7,8 @@
  */
 
 #include "avz.h"
-#include "avz_testing.h"
 #include "avz_logger.h"
+#include "avz_testing.h"
 
 using namespace AvZ;
 using namespace cresc;
@@ -47,15 +47,12 @@ int completed_round = 0;
 int current_jackdata = 0;
 int total_jack_num = 0;
 
-void Script()
-{
+void Script() {
     // 跳帧测试
-    auto condition = [=]()
-    {
+    auto condition = [=]() {
         return current_jackdata < BATCH_SIZE && ice_time_list.size() == BATCH_SIZE;
     };
-    auto callback = [=]()
-    {
+    auto callback = [=]() {
         OpenMultipleEffective('W', -1);
         SetErrorMode(POP_WINDOW);
         SetGameSpeed(0.2);
@@ -67,8 +64,7 @@ void Script()
         freePlanting(false);
 
         // 显示统计结果
-        for (int i = 0; i < BATCH_SIZE; i++)
-        {
+        for (int i = 0; i < BATCH_SIZE; i++) {
             AvZ::ShowErrorNotInQueue("测试完毕，总共测试了#只小丑", total_jack_num);
             Log.log("总共测试了#只小丑。\n#", total_jack_num, jd_list[i].stats());
         }
@@ -85,14 +81,13 @@ void Script()
     SelectCards({ICE_SHROOM, M_ICE_SHROOM, 40, 41, 42, 43, 44, 45, 46, 47});
 
     // 开启修改功能
-    forbidItemDrop();  // 僵尸不掉钱
-    forbidEnterHome(); // 僵尸不进家
-    plantInvincible(); // 植物无敌
-    freePlanting();    // 自由种植
+    forbidItemDrop();   // 僵尸不掉钱
+    forbidEnterHome();  // 僵尸不进家
+    plantInvincible();  // 植物无敌
+    freePlanting();     // 自由种植
 
     // 将小丑僵尸平均分配至指定路
-    if (!jack_rows.empty())
-    {
+    if (!jack_rows.empty()) {
         moveZombieRow(JACK_IN_THE_BOX_ZOMBIE, jack_rows);
     }
 
@@ -107,10 +102,9 @@ void Script()
     killAllZombie({1, 10}, {JACK_IN_THE_BOX_ZOMBIE}, 430);
 
     SetTime(-599, 1);
-    jd_list[current_jackdata].start(test_plant_list); // 开始记录指定植物的炸率数据
-    aa.start(always_attack_list);                     // 使指定植物永动
-    if (LOCK_WAVE == 1)
-    {
+    jd_list[current_jackdata].start(test_plant_list);  // 开始记录指定植物的炸率数据
+    aa.start(always_attack_list);                      // 使指定植物永动
+    if (LOCK_WAVE == 1) {
         lw.start({JACK_IN_THE_BOX_ZOMBIE});
     }
 
@@ -119,18 +113,16 @@ void Script()
 
     // 更新已经完成的选卡数
     InsertTimeOperation(
-        0, 20, [=]()
-        {
-        total_jack_num += countZombie({JACK_IN_THE_BOX_ZOMBIE}, {1, 10, 20});
-        completed_round++;
-        if (completed_round >= TOTAL_TEST_ROUND)
-        {
-            completed_round = 0;
-            current_jackdata++;
-        }
-        if (current_jackdata < BATCH_SIZE && EPOCH > 0 && completed_round % (TOTAL_TEST_ROUND / EPOCH) == 0)
-        {
-            Log.log("总共测试了#只小丑。\n#", total_jack_num, jd_list[current_jackdata].stats());
-        } },
+        0, 20, [=]() {
+            total_jack_num += countZombie({JACK_IN_THE_BOX_ZOMBIE}, {1, 10, 20});
+            completed_round++;
+            if (current_jackdata < BATCH_SIZE && EPOCH > 0 && TOTAL_TEST_ROUND / EPOCH != 0 && completed_round % (TOTAL_TEST_ROUND / EPOCH) == 0) {
+                Log.log("总共测试了#只小丑。\n#", total_jack_num, jd_list[current_jackdata].stats());
+            }
+            if (completed_round >= TOTAL_TEST_ROUND) {
+                completed_round = 0;
+                current_jackdata++;
+            }
+        },
         "w20-stats");
 }
