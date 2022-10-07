96e4904924355522fb04d2aa4154438bd5b21bb2
qrmd <13371005536@163.com>
2022-10-07 23:10:17 +0800

    修复了 showme_bar 的若干 bug

--- vector/showme/example/showme_bar.cpp
+++ vector/showme/example/showme_bar.cpp
@@ -2,7 +2,7 @@
  * @Author: qrmd
  * @Date: 2022-10-02 10:03:02
  * @LastEditors: qrmd
- * @LastEditTime: 2022-10-07 17:44:58
+ * @LastEditTime: 2022-10-07 23:09:07
  * @Description:avz_showme 进度条示例
  * 使用方法：1、前往https://gitee.com/vector-wlc/AsmVsZombies，根据教程下载并安装好AsmVsZombies
  *          2、前往游戏存档文件夹C:/ProgramData/PopCap Games/PlantsVsZombies/userdata，备份原游戏存档，然后用脚本配套的存档文件替换同名文件
@@ -15,10 +15,9 @@
 #include "avz.h"
 #include "showme/sm.h"
 
-int bar_width = 100;    // 水平进度条的宽度（像素）
-int hp_bar_height = 15; // 水平生命条的高度（像素）
-int bar_height = 6;     // 水平进度条的高度（像素）
-int frame_size = 1;     // 边框粗细（像素）
+int bar_width = 100; // 水平进度条的宽度（像素）
+int bar_height = 6;  // 水平进度条的高度（像素）
+int frame_size = 1;  // 边框粗细（像素）
 
 int JACK_IN_THE_BOX_OPEN_BOX_TIME = 110;         // 小丑僵尸开盒时长
 int BUNGEE_REMAIN_TIME = 300;                    // 蹦极僵尸停留时长
@@ -151,55 +150,55 @@ void Draw()
                 index_animation = zombies[index].animationCode();
                 circulation_rate = animations[index_animation].circulationRate();
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // 读报僵尸失去报纸
             if (zombies[index].type() == NEWSPAPER_ZOMBIE && zombies[index].state() == 30) {
                 index_animation = zombies[index].animationCode();
                 circulation_rate = animations[index_animation].circulationRate();
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // // 舞王僵尸和伴舞僵尸跳舞
             // if ((zombies[index].type() == DANCING_ZOMBIE || zombies[index].type() == BACKUP_DANCER) && zombies[index].state() >= 44 && zombies[index].state() <= 49) {
             //     index_animation = zombies[index].animationCode();
             //     circulation_rate = animations[index_animation].circulationRate();
 
-            //     bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+            //     bar_painter.drawBar(bar_pos_x, bar_pos_y,circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             // }
             // 潜水僵尸入水
             if (zombies[index].type() == SNORKEL_ZOMBIE && zombies[index].state() == 58) {
                 index_animation = zombies[index].animationCode();
                 circulation_rate = animations[index_animation].circulationRate();
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // 海豚僵尸入水和跳跃
             if (zombies[index].type() == DOLPHIN_RIDER_ZOMBIE && (zombies[index].state() == 52 || zombies[index].state() == 54)) {
                 index_animation = zombies[index].animationCode();
                 circulation_rate = animations[index_animation].circulationRate();
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // 玩偶匣僵尸开盒
             if (zombies[index].type() == JACK_IN_THE_BOX_ZOMBIE && zombies[index].state() == 16) {
                 circulation_rate = 1 - zombies[index].stateCountdown() / float(JACK_IN_THE_BOX_OPEN_BOX_TIME);
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // 气球僵尸落地
             if (zombies[index].type() == BALLOON_ZOMBIE && zombies[index].state() == 74) {
                 index_animation = zombies[index].animationCode();
                 circulation_rate = animations[index_animation].circulationRate();
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // 矿工僵尸出土后眩晕
             if (zombies[index].type() == DIGGER_ZOMBIE && zombies[index].state() == 36) {
                 index_animation = zombies[index].animationCode();
                 circulation_rate = (animations[index_animation].circulationRate() + animations[index_animation].circulationCount()) / 2.0;
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // 蹦极僵尸停留
             if (zombies[index].type() == BUNGEE_ZOMBIE && (zombies[index].state() == 6 || zombies[index].state() == 7)) {
@@ -210,27 +209,27 @@ void Draw()
                     circulation_rate = (BUNGEE_REMAIN_TIME + animations[index_animation].circulationRate() * BUNGEE_GRAB_TIME) / float(BUNGEE_REMAIN_TIME + BUNGEE_GRAB_TIME);
                 }
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {300.0 / (300 + 63)});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {300.0 / (300 + 63)});
             }
             // 投篮僵尸投篮
             if (zombies[index].type() == CATAPULT_ZOMBIE && (zombies[index].state() == 67 || zombies[index].state() == 68)) {
                 circulation_rate = 1 - zombies[index].stateCountdown() / float(SHOOT_BASKETBALL_COUNTDOWN);
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {SHOOT_BASKETBALL_CIRCULATION_RATE});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {SHOOT_BASKETBALL_CIRCULATION_RATE});
             }
             // 巨人僵尸锤击
             if ((zombies[index].type() == GIGA_GARGANTUAR || zombies[index].type() == GARGANTUAR) && zombies[index].isHammering()) {
                 index_animation = zombies[index].animationCode();
                 circulation_rate = animations[index_animation].circulationRate();
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {HAMMER_CIRCULATION_RATE});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {HAMMER_CIRCULATION_RATE});
             }
             // 巨人僵尸投掷小鬼
             if ((zombies[index].type() == GIGA_GARGANTUAR || zombies[index].type() == GARGANTUAR) && zombies[index].state() == 69) {
                 index_animation = zombies[index].animationCode();
                 circulation_rate = animations[index_animation].circulationRate();
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {THROW_IMP_CIRCULATION_RATE});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {THROW_IMP_CIRCULATION_RATE});
             }
             // 僵尸冰冻倒计时
             bar_pos_y = int(zombies[index].ordinate() + zombies[index].bulletOrdinate() - (2 * bar_height + 4 * frame_size));
@@ -238,18 +237,18 @@ void Draw()
             if (zombies[index].freezeCountdown() > 0) {
                 circulation_rate = zombies[index].freezeCountdown() / float(FREEZE_TIME);
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0x0E, 0xA0, 0xE0), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0x0E, 0xA0, 0xE0), SMPainter::RIGHT, {});
             }
 
             // 僵尸血量（不考虑二类饰品）
             bar_pos_y = int(zombies[index].ordinate() + zombies[index].bulletOrdinate() - (bar_height + 2 * frame_size));
             circulation_rate = (zombies[index].hp() + zombies[index].oneHp()) / float(zombies[index].hpMax() + zombies[index].oneHpMax());
             if (zombies[index].type() == GARGANTUAR) {
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, circulation_rate, {1800 / 3000.0});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, {1800 / 3000.0});
             } else if (zombies[index].type() == GIGA_GARGANTUAR) {
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, circulation_rate, {1800 / 6000.0, 1800 * 2 / 6000.0, 1800 * 3 / 6000.0});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, {1800 / 6000.0, 1800 * 2 / 6000.0, 1800 * 3 / 6000.0});
             } else {
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, bar_width, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, bar_width, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, {});
             }
         }
     }
@@ -264,7 +263,7 @@ void Draw()
                 if (plants[index].explodeCountdown() > 0) {
                     circulation_rate = plants[index].explodeCountdown() / 100.0;
 
-                    bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                    bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
                 }
             }
             // 咖啡豆生效进度条（咖啡豆消失后进度条不显示，待完善）
@@ -272,7 +271,7 @@ void Draw()
                 if (plants[index].explodeCountdown() > 0) {
                     circulation_rate = (plants[index].explodeCountdown() + plants[index].bloverCountdown()) / 299.0;
 
-                    bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                    bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
                 }
             }
             // 向日葵、双子向日葵、阳光菇和金盏花生产进度条
@@ -280,20 +279,20 @@ void Draw()
                 if (plants[index].produceCountdown() > 0) {
                     circulation_rate = plants[index].produceCountdown() / 2500.0;
 
-                    bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                    bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
                 }
             }
             // 土豆地雷准备进度条
             if (plants[index].type() == POTATO_MINE && plants[index].state() != 16) {
                 circulation_rate = plants[index].stateCountdown() / 1500.0;
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // 大嘴花消化进度条
             if (plants[index].type() == CHOMPER && plants[index].state() == 13) {
                 circulation_rate = plants[index].stateCountdown() / 4000.0;
 
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // 倭瓜砸击进度条
             if (plants[index].type() == SQUASH) {
@@ -307,7 +306,7 @@ void Draw()
                     } else if (plants[index].state() == 6) {
                         circulation_rate = (plants[index].stateCountdown()) / float(80 + 45 + 50 + 7);
                     }
-                    bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                    bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
                 }
             }
             // 墓碑吞噬者消失进度条
@@ -315,7 +314,7 @@ void Draw()
                 if (plants[index].stateCountdown() > 0) {
                     circulation_rate = plants[index].stateCountdown() / 400.0;
 
-                    bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                    bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
                 }
             }
             // 三叶草生效进度条
@@ -323,7 +322,7 @@ void Draw()
                 if (plants[index].bloverCountdown() > 0) {
                     circulation_rate = (plants[index].explodeCountdown() + plants[index].bloverCountdown()) / 250.0;
 
-                    bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {(250 - 50) / 250.0});
+                    bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {(250 - 50) / 250.0});
                 }
             }
             // 磁力菇复用进度条
@@ -331,7 +330,7 @@ void Draw()
                 if (plants[index].stateCountdown() > 0) {
                     circulation_rate = plants[index].stateCountdown() / 1500.0;
 
-                    bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                    bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
                 }
             }
             // 玉米加农炮复用进度条
@@ -351,15 +350,20 @@ void Draw()
                     circulation_rate = 0;
                     break;
                 }
-                bar_painter.drawBar(bar_pos_x + 40, bar_pos_y, 2 * 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x + 40, bar_pos_y, circulation_rate, 2 * 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
             // 植物血量
             bar_pos_y = plants[index].yi();
             circulation_rate = plants[index].hp() / float(plants[index].hpMax());
             if (plants[index].type() == COB_CANNON) {
-                bar_painter.drawBar(bar_pos_x + 40, bar_pos_y, 2 * 72, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x + 40, bar_pos_y, circulation_rate, 2 * 72, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, {});
             } else {
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                if (plants[index].type() == LILY_PAD || plants[index].type() == FLOWER_POT) {
+                    bar_pos_y = bar_pos_y = plants[index].yi() + (2 * bar_height + 4 * frame_size);
+                } else if (plants[index].type() == PUMPKIN || plants[index].type() == COFFEE_BEAN) {
+                    bar_pos_y = bar_pos_y = plants[index].yi() + (4 * bar_height + 8 * frame_size);
+                }
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0x00, 0xFF, 0x00), SMPainter::RIGHT, {});
             }
         }
     }
@@ -372,7 +376,7 @@ void Draw()
                 bar_pos_x = x_diff * (groundItems[index].col() + 1);
                 bar_pos_y = y_diff * (groundItems[index].row() + 1) + 10;
                 circulation_rate = groundItems[index].countdown() / CRATER_REMAINING_TIME;
-                bar_painter.drawBar(bar_pos_x, bar_pos_y, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, circulation_rate, {});
+                bar_painter.drawBar(bar_pos_x, bar_pos_y, circulation_rate, 72, bar_height, 1, SMArgb(0xFF, 0xFF, 0xC0, 0x00), SMPainter::RIGHT, {});
             }
         }
     }
