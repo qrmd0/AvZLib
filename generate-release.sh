581c6559ee8053510f4e6d38dd5e1071d66d6a93
alumkal <alumkal-pub@outlook.com>
2023-04-05 13:53:49 +0800

    vector/avz1: release 2023_04_05

--- generate-release.sh
+++ generate-release.sh
@@ -3,4 +3,7 @@ version=$(echo $@ | gawk 'match($0, /(.*): release (.*)/, a) { print(a[2]) }')
 cd $folder
 mkdir -p release/
 zip -r release/$version.zip $(basename $folder)/
+if [[ -s version.txt && `tail -c1 version.txt | wc -l` == 0 ]]; then
+    echo "" >> version.txt
+fi
 echo $version >> version.txt
