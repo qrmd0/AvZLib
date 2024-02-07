bfc194841ef5e2521e87c6dac9d8ee4058dfed1e
Rottenham <54915258+Rottenham@users.noreply.github.com>
2024-02-06 20:05:09 -0800

    crescendo/SimpleAvZ: release 1.0.1

--- crescendo/SimpleAvZ/SimpleAvZ/error.h
+++ crescendo/SimpleAvZ/SimpleAvZ/error.h
@@ -8,7 +8,7 @@ template <typename... Args>
 void error(const std::string& func_name, const std::string& content, Args... args)
 {
     AvZ::SetErrorMode(AvZ::POP_WINDOW);
-    AvZ::ShowErrorNotInQueue("[Error when calling " + func_name + " ]\n" + content, args...);
+    AvZ::ShowErrorNotInQueue("[调用 " + func_name + " 时出错]\n" + content, args...);
 
     throw "";
 }
